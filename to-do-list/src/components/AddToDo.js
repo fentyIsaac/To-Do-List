ec86cbbb856a806aec1277fd7c11270f75767746
Isaac Fenty <piggottfentyi@students.westerntc.edu>
2024-10-23 17:16:52 -0500

    Added add to-do functionality

---
+++ to-do-list/src/components/AddToDo.js
@@ -0,0 +1,23 @@
+const AddTodo = ({ addTask }) => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task) {
+      addTask(task);
+      setTask("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
